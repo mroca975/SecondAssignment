2e23f0b3ba135ab507c09f96a68492cb020e559f
mroca <mroca975@hotmail.com>
2021-04-03 21:46:45 -0300

    Se agrega un comentario

--- SecondAssignment/src/main/java/es/smartcoding/secondassignment/App.java
+++ SecondAssignment/src/main/java/es/smartcoding/secondassignment/App.java
@@ -1,5 +1,12 @@
 package es.smartcoding.secondassignment;
 
+/**
+ * 
+ * @param a
+ * @param b
+ * @return la suma de a y b
+ * @throws Exception
+ */
 public interface App 
 {
 	public abstract int sum(final int a, final int b) throws Exception;
